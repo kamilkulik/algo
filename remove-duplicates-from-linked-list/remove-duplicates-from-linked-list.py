8e4361ef85270c45e93377451337272ea4d6ee8c
kamilkulikTeck <kulikkamil@gmail.com>
2021-06-24 10:36:20 +0200

    remove-duplicates-from-linked-list

---
+++ remove-duplicates-from-linked-list/remove-duplicates-from-linked-list.py
@@ -0,0 +1,37 @@
+# Solution 1
+# iterate over every node of the linekd list and remove duplicates one by one
+# O(n) time and O(1) space
+
+def removeDuplicatesFromLinkedList(linkedList):
+	prev = None
+	curr = linkedList
+	
+	while curr is not None:
+		if prev and curr.value == prev.value:
+			prev.next = curr.next
+			curr = curr.next
+			
+		else:
+			prev = curr
+			curr = curr.next
+			
+	return linkedList
+
+# Solution 2
+# iterate over nodes, when next node's value is same as current node's, loop over subsequent node to find the first unique one and so on
+# O(n) time and O(1) space
+
+def removeDuplicatesFromLinkedList(linked_list):
+    current_node = linked_list
+	
+    while current_node is not None:
+        next_unique_node = current_node.next
+        
+        while next_unique_node is not None and current_node.value == next_unique_node.value:
+          next_unique_node = next_unique_node.next
+        
+        current_node.next = next_unique_node
+        current_node = next_unique_node
+        
+        
+        return linked_list
