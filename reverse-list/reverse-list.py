0e1353be9b26b9cd0d5ba70a8aae85a99ba49b4a
Kamil <kulikkamil@gmail.com>
2021-07-04 18:14:26 +0200

    three-largest_numbers

--- reverse-list/reverse-list.py
+++ reverse-list/reverse-list.py
@@ -1,22 +1,22 @@
 list_for_in_place_reversal = [4, 5, 6, 7, 9, 11, 435, 45, 1]
 
 def reverse_list_in_place(list):
-	start = 0
-	end = len(list) - 1
+		start = 0
+		end = len(list) - 1
 
-	while start < end:
-		list[start], list[end] = list[end], list[start]
-		start += 1
-		end -= 1
+		while start < end:
+				list[start], list[end] = list[end], list[start]
+				start += 1
+				end -= 1
 
 reverse_list_in_place(list_for_in_place_reversal)
 print(list_for_in_place_reversal)
 
 
 def reverse_list_recursively(list):
-	if len(list) == 0: return []
+		if len(list) == 0: return []
 
-	return [list[-1]] + reverse_list_recursively(list[: -1])
+		return [list[-1]] + reverse_list_recursively(list[: -1])
 
 
 print(reverse_list_recursively([4, 5, 6, 7, 9, 11, 435, 45, 1]))
