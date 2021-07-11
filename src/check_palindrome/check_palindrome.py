e6f03e3c8d9f660e759753cd9e31e57f9f819913
kamilkulikTeck <kulikkamil@gmail.com>
2021-07-11 17:53:22 +0200

    add check_palindrome variation using array to create reversed string

--- src/check_palindrome/check_palindrome.py
+++ src/check_palindrome/check_palindrome.py
@@ -1,5 +1,21 @@
-def check_palindrome_string_concat(string):
+
+# TIME O(n^2), because since strings are immutable in Python
+# string concatination is an O(n^2) operation
+# BECAUSE interpreter needs to first allocate space for the new string and then create it
+# SPACE O(n)
+
+def check_palindrome_reversed_string_concat(string):
     new_string = ''
     for i in reversed(range(len(string))):
         new_string += string[i]
     return new_string == string
+
+# TIME O(n) because we're now appending to the end of array
+# which is an O(n) opearation
+# SPACE O(n)
+
+def check_palindrome_reversed_string_array(string):
+    new_string = []
+    for i in reversed(range(len(string))):
+        new_string.append(string[i])
+    return ''.join(new_string) == string
--- tests/check_palindrome_test.py
+++ tests/check_palindrome_test.py
@@ -1,7 +1,14 @@
-from src.check_palindrome.check_palindrome import check_palindrome_string_concat
+from src.check_palindrome.check_palindrome import check_palindrome_reversed_string_concat, check_palindrome_reversed_string_array
 
-def test_check_palindrome_string_concat_1():
-    assert check_palindrome_string_concat('kayak') == True
+def test_check_palindrome_reversed_string_concat_1():
+    assert check_palindrome_reversed_string_concat('kayak') == True
     
-def test_check_palindrome_string_concat_2():
-    assert check_palindrome_string_concat('kadyak') == False
+def test_check_palindrome_reversed_string_concat_2():
+    assert check_palindrome_reversed_string_concat('kadyak') == False
+
+def test_check_palindrome_reversed_string_array_1():
+    assert check_palindrome_reversed_string_array('kayak') == True
+    
+def test_check_palindrome_reversed_string_array_2():
+    assert check_palindrome_reversed_string_array('kadyak') == False
+
