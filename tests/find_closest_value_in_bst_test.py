41c05140a35e0d07429ce4c7ba14661fb77e18d9
Kamil <kulikkamil@gmail.com>
2021-07-21 09:18:48 +0200

    add tests to find_closest_value_in_bst_test

--- tests/find_closest_value_in_bst_test.py
+++ tests/find_closest_value_in_bst_test.py
@@ -2,33 +2,43 @@ from src.find_closest_value_in_bst.find_closest_value_in_bst import find_closest
 from src.binary_search_tree.binary_search_tree import BST
 
 def build_binary_tree(tree):
-    binary_tree = BST(tree["tree"]["nodes"][0]["value"])
-    remaining_nodes = tree["tree"]["nodes"][1:]
+    binary_tree = BST(tree["root"])
+    remaining_nodes = tree["nodes"]
 
     for node in remaining_nodes:
-        binary_tree.insert(node["value"])
+        binary_tree.insert(node)
 
     return binary_tree
 
 def test_find_closest_value_in_bst_1():
+    
     tree = {
-        "tree": {
-            "nodes": [
-            {"id": "10", "left": "5", "right": "15", "value": 10},
-            {"id": "15", "left": "13", "right": "22", "value": 15},
-            {"id": "22", "left": None, "right": None, "value": 22},
-            {"id": "13", "left": None, "right": "14", "value": 13},
-            {"id": "14", "left": None, "right": None, "value": 14},
-            {"id": "5", "left": "2", "right": "5-2", "value": 5},
-            {"id": "5-2", "left": None, "right": None, "value": 5},
-            {"id": "2", "left": "1", "right": None, "value": 2},
-            {"id": "1", "left": None, "right": None, "value": 1}
-            ],
-            "root": "10"
-        },
-        "target": 12
+        "root": 10,
+        "nodes": [15, 22, 13, 14, 5, 5, 2, 1]
     }
     
     binary_tree = build_binary_tree(tree)
 
     assert find_closest_value_in_bst(binary_tree, 12) == 13
+
+def test_find_closest_value_in_bst_2():
+    
+    tree = {
+        "root": 100,
+        "nodes": [502, 55000, 1001, 4500, 204, 205, 207, 208, 206, 203, 5, 15, 22, 57, 60, 5, 2, 3, 1, 1, 1, 1, 1, -51, -403]
+    }
+    
+    binary_tree = build_binary_tree(tree)
+
+    assert find_closest_value_in_bst(binary_tree, 100) == 100
+    
+def test_find_closest_value_in_bst_3():
+    
+    tree = {
+        "root": 100,
+        "nodes": [502, 55000, 1001, 4500, 204, 205, 207, 208, 206, 203, 5, 15, 22, 57, 60, 5, 2, 3, 1, 1, 1, 1, 1, -51, -403]
+    }
+    
+    binary_tree = build_binary_tree(tree)
+
+    assert find_closest_value_in_bst(binary_tree, -70) == -51
