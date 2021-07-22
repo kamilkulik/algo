2decda973240b2606477b842e64dcb4031e56179
Kamil <kulikkamil@gmail.com>
2021-07-22 08:08:20 +0200

    TDD driven tests for for loop based validate_subsequence algo

--- tests/validate_subsequence_test.py
+++ tests/validate_subsequence_test.py
@@ -1,4 +1,5 @@
 from src.validate_subsequence.validate_subsequence import validate_subsequence
+from src.validate_subsequence.validate_subsequence import validate_subquence_for_loop
 
 def test_validate_subsequence_1():
     assert validate_subsequence([1, 2, 3, 4], [2, 3]) == True
@@ -14,4 +15,19 @@ def test_validate_subsequence_4():
 
 def test_validate_subsequence_5():
     assert validate_subsequence([5, 1, 22, 25, 6, -1, 8, 10], [1, 6, -1, 10, 11, 11, 11, 11]) == False
+
+def test_validate_subquence_for_loop_1():
+    assert validate_subquence_for_loop([1, 2, 3, 4], [2, 3]) == True
+
+def test_validate_subquence_for_loop_2():
+    assert validate_subquence_for_loop([5, 1, 22, 25, 6, -1, 8, 10], [22, 25, 6]) == True
+
+def test_validate_subquence_for_loop_3():
+    assert validate_subquence_for_loop([1, 1, 1, 1, 1], [1, 1, 1]) == True
+
+def test_validate_subquence_for_loop_4():
+    assert validate_subquence_for_loop([5, 1, 22, 25, 6, -1, 8, 10], [5, 1, 22, 25, 6, -1, 8, 10, 12]) == False
+
+def test_validate_subquence_for_loop_5():
+    assert validate_subquence_for_loop([5, 1, 22, 25, 6, -1, 8, 10], [1, 6, -1, 10, 11, 11, 11, 11]) == False
 
