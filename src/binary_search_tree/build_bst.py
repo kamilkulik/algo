57698bc0e1348f7d53a6fade22ecc1aad34fd4fc
Kamil <kulikkamil@gmail.com>
2021-07-28 07:54:07 +0200

    relocate the build_binary_tree function

---
+++ src/binary_search_tree/build_bst.py
@@ -0,0 +1,10 @@
+from src.binary_search_tree.binary_search_tree import BST
+
+def build_binary_tree(tree):
+    binary_tree = BST(tree["root"])
+    remaining_nodes = tree["nodes"]
+
+    for node in remaining_nodes:
+        binary_tree.insert(node)
+
+    return binary_tree
