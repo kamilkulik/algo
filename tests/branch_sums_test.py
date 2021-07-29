3afd4c0f0184e605b88036b58539ee97dbf73568
Kamil <kulikkamil@gmail.com>
2021-07-29 09:25:07 +0200

    refactor branch_sums tests

--- tests/branch_sums_test.py
+++ tests/branch_sums_test.py
@@ -1,11 +1,24 @@
 from src.branch_sums.branch_sums import branch_sums
-from src.binary_search_tree.binary_search_tree import BST
-from src.binary_search_tree.build_bst import build_binary_tree
+from src.binary_tree.build_bt import build_binary_tree
+from src.binary_tree.binary_tree import BT
 
 def test_branch_sums_1():
     tree = {
-        "root": 1,
-        "nodes": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        "tree": {
+            "nodes": [
+            {"id": "1", "left": "2", "right": "3", "value": 1},
+            {"id": "2", "left": "4", "right": "5", "value": 2},
+            {"id": "3", "left": "6", "right": "7", "value": 3},
+            {"id": "4", "left": "8", "right": "9", "value": 4},
+            {"id": "5", "left": "10", "right": None, "value": 5},
+            {"id": "6", "left": None, "right": None, "value": 6},
+            {"id": "7", "left": None, "right": None, "value": 7},
+            {"id": "8", "left": None, "right": None, "value": 8},
+            {"id": "9", "left": None, "right": None, "value": 9},
+            {"id": "10", "left": None, "right": None, "value": 10}
+            ],
+            "root": "1"
+        }
     }
 
     binary_tree = build_binary_tree(tree)
@@ -14,8 +27,24 @@ def test_branch_sums_1():
     
 def test_branch_sums_2():
     tree = {
-        "root": 1,
-        "nodes": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 1, 1]
+        "tree": {
+            "nodes": [
+            {"id": "1", "left": "2", "right": "3", "value": 1},
+            {"id": "2", "left": "4", "right": "5", "value": 2},
+            {"id": "3", "left": "6", "right": "7", "value": 3},
+            {"id": "4", "left": "8", "right": "9", "value": 4},
+            {"id": "5", "left": "10", "right": "1-2", "value": 5},
+            {"id": "6", "left": "1-3", "right": "1-4", "value": 6},
+            {"id": "7", "left": None, "right": None, "value": 7},
+            {"id": "8", "left": None, "right": None, "value": 8},
+            {"id": "9", "left": None, "right": None, "value": 9},
+            {"id": "10", "left": None, "right": None, "value": 10},
+            {"id": "1-2", "left": None, "right": None, "value": 1},
+            {"id": "1-3", "left": None, "right": None, "value": 1},
+            {"id": "1-4", "left": None, "right": None, "value": 1}
+            ],
+            "root": "1"
+        }
     }
 
     binary_tree = build_binary_tree(tree)
@@ -24,8 +53,18 @@ def test_branch_sums_2():
 
 def test_branch_sums_3():
     tree = {
-        "root": 0,
-        "nodes": [0, 9, 1, 15, 10, 100, 200]
+        "tree": {
+            "nodes": [
+            {"id": "0", "left": "9", "right": "1", "value": 0},
+            {"id": "9", "left": None, "right": None, "value": 9},
+            {"id": "1", "left": "15", "right": "10", "value": 1},
+            {"id": "15", "left": None, "right": None, "value": 15},
+            {"id": "10", "left": "100", "right": "200", "value": 10},
+            {"id": "100", "left": None, "right": None, "value": 100},
+            {"id": "200", "left": None, "right": None, "value": 200}
+            ],
+            "root": "0"
+        }
     }
 
     binary_tree = build_binary_tree(tree)
--- tests/find_closest_value_in_bst_test.py
+++ tests/find_closest_value_in_bst_test.py
@@ -1,6 +1,6 @@
 from src.find_closest_value_in_bst.find_closest_value_in_bst import find_closest_value_in_bst
 from src.binary_search_tree.binary_search_tree import BST
-from src.binary_search_tree.build_bst import build_binary_tree
+from src.binary_search_tree.build_bst import build_binary_search_tree
 
 def test_find_closest_value_in_bst_1():
     
@@ -9,7 +9,7 @@ def test_find_closest_value_in_bst_1():
         "nodes": [15, 22, 13, 14, 5, 5, 2, 1]
     }
     
-    binary_tree = build_binary_tree(tree)
+    binary_tree = build_binary_search_tree(tree)
 
     assert find_closest_value_in_bst(binary_tree, 12) == 13
 
@@ -20,7 +20,7 @@ def test_find_closest_value_in_bst_2():
         "nodes": [502, 55000, 1001, 4500, 204, 205, 207, 208, 206, 203, 5, 15, 22, 57, 60, 5, 2, 3, 1, 1, 1, 1, 1, -51, -403]
     }
     
-    binary_tree = build_binary_tree(tree)
+    binary_tree = build_binary_search_tree(tree)
 
     assert find_closest_value_in_bst(binary_tree, 100) == 100
     
@@ -31,6 +31,6 @@ def test_find_closest_value_in_bst_3():
         "nodes": [502, 55000, 1001, 4500, 204, 205, 207, 208, 206, 203, 5, 15, 22, 57, 60, 5, 2, 3, 1, 1, 1, 1, 1, -51, -403]
     }
     
-    binary_tree = build_binary_tree(tree)
+    binary_tree = build_binary_search_tree(tree)
 
     assert find_closest_value_in_bst(binary_tree, -70) == -51
