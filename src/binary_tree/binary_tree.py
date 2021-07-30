082e40b0f85af26fa4058a677e133b6e833008c0
Kamil <kulikkamil@gmail.com>
2021-07-30 07:42:03 +0200

    cleanup in binary_tree.py

--- src/binary_tree/binary_tree.py
+++ src/binary_tree/binary_tree.py
@@ -54,32 +54,3 @@ class BT:
     #         found_node.append(node)
     #     self.find_next_node(node.left, id, found_node)
     #     self.find_next_node(node.right, id, found_node)
-
-tree = {
-    "tree": {
-        "nodes": [
-        {"id": "1", "left": "2", "right": "3", "value": 1},
-        {"id": "2", "left": "4", "right": "5", "value": 2},
-        {"id": "3", "left": "6", "right": "7", "value": 3},
-        {"id": "4", "left": "8", "right": "9", "value": 4},
-        {"id": "5", "left": "10", "right": None, "value": 5},
-        {"id": "6", "left": None, "right": None, "value": 6},
-        {"id": "7", "left": None, "right": None, "value": 7},
-        {"id": "8", "left": None, "right": None, "value": 8},
-        {"id": "9", "left": None, "right": None, "value": 9},
-        {"id": "10", "left": None, "right": None, "value": 10}
-        ],
-        "root": "1"
-    }
-}
-
-def build_binary_tree(tree):
-    binary_tree = BT(tree["tree"]["root"])
-    remaining_nodes = tree["tree"]["nodes"]
-
-    for node in remaining_nodes:
-        binary_tree.insert(node["id"], node["left"], node["right"], node["value"])
-
-    return binary_tree
-
-print(build_binary_tree(tree))
