65961045c1ac5e156c75238092d524943d898688
Kamil <kulikkamil@gmail.com>
2021-08-06 08:28:24 +0200

    add test cases for depth_first_search algo

--- tests/depth_first_search_test.py
+++ tests/depth_first_search_test.py
@@ -1,7 +1,7 @@
 from src.depth_first_search.depth_first_search import Node
 from src.graph.graph import build_graph
             
-def test_depth_first_search():
+def test_depth_first_search_1():
     graph = {
         "graph": {
             "nodes": [
@@ -28,3 +28,67 @@ def test_depth_first_search():
     build_graph(complete_graph, nodes)
 
     assert complete_graph.depth_first_search([]) == ["A", "B", "E", "F", "I", "J", "C", "D", "G", "K", "H"]
+
+def test_depth_first_search_2():
+    graph = {
+        "graph": {
+            "nodes": [
+            {"children": ["B", "C", "D", "E", "F"], "id": "A", "value": "A"},
+            {"children": ["G", "H", "I"], "id": "B", "value": "B"},
+            {"children": ["J"], "id": "C", "value": "C"},
+            {"children": ["K", "L"], "id": "D", "value": "D"},
+            {"children": [], "id": "E", "value": "E"},
+            {"children": ["M", "N"], "id": "F", "value": "F"},
+            {"children": [], "id": "G", "value": "G"},
+            {"children": ["O", "P", "Q", "R"], "id": "H", "value": "H"},
+            {"children": [], "id": "I", "value": "I"},
+            {"children": [], "id": "J", "value": "J"},
+            {"children": ["S"], "id": "K", "value": "K"},
+            {"children": [], "id": "L", "value": "L"},
+            {"children": [], "id": "M", "value": "M"},
+            {"children": [], "id": "N", "value": "N"},
+            {"children": [], "id": "O", "value": "O"},
+            {"children": ["T", "U"], "id": "P", "value": "P"},
+            {"children": [], "id": "Q", "value": "Q"},
+            {"children": ["V"], "id": "R", "value": "R"},
+            {"children": [], "id": "S", "value": "S"},
+            {"children": [], "id": "T", "value": "T"},
+            {"children": [], "id": "U", "value": "U"},
+            {"children": ["W", "X", "Y"], "id": "V", "value": "V"},
+            {"children": [], "id": "W", "value": "W"},
+            {"children": ["Z"], "id": "X", "value": "X"},
+            {"children": [], "id": "Y", "value": "Y"},
+            {"children": [], "id": "Z", "value": "Z"}
+            ],
+            "startNode": "A"
+        }
+    }
+
+    start_node = graph["graph"]["startNode"]
+    nodes = graph["graph"]["nodes"]
+
+    complete_graph = Node(start_node)
+    build_graph(complete_graph, nodes)
+
+    assert complete_graph.depth_first_search([]) == ["A", "B", "G", "H", "O", "P", "T", "U", "Q", "R", "V", "W", "X", "Z", "Y", "I", "C", "J", "D", "K", "S", "L", "E", "F", "M", "N"]
+
+def test_depth_first_search_3():
+    graph = {
+        "graph": {
+            "nodes": [
+            {"children": ["B", "C"], "id": "A", "value": "A"},
+            {"children": ["D"], "id": "B", "value": "B"},
+            {"children": [], "id": "C", "value": "C"},
+            {"children": [], "id": "D", "value": "D"}
+            ],
+            "startNode": "A"
+        }
+    }
+
+    start_node = graph["graph"]["startNode"]
+    nodes = graph["graph"]["nodes"]
+
+    complete_graph = Node(start_node)
+    build_graph(complete_graph, nodes)
+
+    assert complete_graph.depth_first_search([]) == ["A", "B", "D", "C"]
