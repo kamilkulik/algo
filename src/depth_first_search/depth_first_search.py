6391d1b37715bd59a21e9a6c8cde1cac3f0ce01f
Kamil <kulikkamil@gmail.com>
2021-08-06 08:53:34 +0200

    add time and space complexity to depth_first_search algo

--- src/depth_first_search/depth_first_search.py
+++ src/depth_first_search/depth_first_search.py
@@ -7,6 +7,9 @@ class Node():
         self.children.append(Node(id))
         return self
 
+    # Complexity
+    # Time: O(V + E) where: V are vertexes of the graph and E are its edges
+    # Space: O(V) meaning the array of results will have length equal to number of vertexes
     def depth_first_search(self, array):
         array.append(self.id)
         for child in self.children:
