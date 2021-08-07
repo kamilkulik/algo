fefeb5d90f942b9b639f1f0b59ff6932ab2d2394
Kamil <kulikkamil@gmail.com>
2021-08-07 22:16:47 +0200

    refactor binary_search algo tests to use @pytest.mark.parametrize() decorator

--- tests/binary_search_test.py
+++ tests/binary_search_test.py
@@ -1,17 +1,16 @@
 from src.binary_search.binary_search import binarySearch
+import pytest
 
-def test_binary_search_1():
-    assert binarySearch([1, 4, 5, 7, 8, 10, 13, 16, 19, 20, 26, 43, 48, 49, 50, 53, 55, 59, 64, 67, 80], 26) == 10
+test_cases = [
+    ([1, 4, 5, 7, 8, 10, 13, 16, 19, 20, 26, 43, 48, 49, 50, 53, 55, 59, 64, 67, 80], 26, 10),
+    ([1, 4, 5, 7, 8, 10, 13, 16, 19, 20, 26, 43, 48, 49, 50, 53, 55, 59, 64, 67, 80], 47, -1),
+    ([4, 5, 7, 8, 10, 13, 16, 19, 20, 26, 43, 48, 49, 50, 53, 55, 59, 64, 67, 80], 4, 0),
+    ([4, 5, 7, 8, 10, 13, 16, 19, 20, 26, 43, 48, 49, 50, 53, 55, 59, 64, 67, 80], 80, 19),
+    ([-8, -6, -5, 0, 4, 5, 7, 8, 10, 13, 16, 19, 20, 26, 43, 48, 49, 50, 53, 55, 59, 64, 67, 80], 80, 23)
+]
 
-def test_binary_search_2():
-    assert binarySearch([1, 4, 5, 7, 8, 10, 13, 16, 19, 20, 26, 43, 48, 49, 50, 53, 55, 59, 64, 67, 80], 47) == -1
+ids = ['Input: {}, Target: {}, Output: {}'.format(case[0], case[1], case[2]) for case in test_cases]
 
-def test_binary_search_3():
-    assert binarySearch([4, 5, 7, 8, 10, 13, 16, 19, 20, 26, 43, 48, 49, 50, 53, 55, 59, 64, 67, 80], 4) == 0
-
-def test_binary_search_4():
-    assert binarySearch([4, 5, 7, 8, 10, 13, 16, 19, 20, 26, 43, 48, 49, 50, 53, 55, 59, 64, 67, 80], 80) == 19
-
-def test_binary_search_5():
-    assert binarySearch([-8, -6, -5, 0, 4, 5, 7, 8, 10, 13, 16, 19, 20, 26, 43, 48, 49, 50, 53, 55, 59, 64, 67, 80], 80) == 23
-
+@pytest.mark.parametrize('array, target, index', test_cases, ids=ids)
+def test_binary_search(array, target, index):
+    assert binarySearch(array, target) == index
