604d79496b560a468c640afcff4d13bd450cb66c
Kamil <kulikkamil@gmail.com>
2021-08-08 12:26:29 +0200

    refactor class_photos_test to use test parametrization

--- tests/class_photos_test.py
+++ tests/class_photos_test.py
@@ -1,31 +1,17 @@
 from src.class_photos.class_photos import class_photos
+import pytest
 
-def tet_class_photos_1():
-    red_shirt_heights = [6]
-    blue_shirt_heights = [6]
-    
-    assert class_photos(red_shirt_heights, blue_shirt_heights) == False
-    
-def tet_class_photos_2():
-    red_shirt_heights = [6, 9, 2, 4, 5, 1]
-    blue_shirt_heights = [5, 8, 1, 3, 4, 9]
-    
-    assert class_photos(red_shirt_heights, blue_shirt_heights) == False
+test_cases = [
+    ([6], [6], False),
+    ([6, 9, 2, 4, 5, 1], [5, 8, 1, 3, 4, 9], False),
+    ([1, 2, 3, 4, 5], [2, 3, 4, 5, 6], True),
+    ([1, 1, 1, 1, 1, 1, 1, 1], [2, 2, 2, 2, 2, 2, 2, 2], True),
+    ([19, 2, 4, 6, 2, 3, 1, 1, 4], [21, 5, 4, 4, 4, 4, 4, 4, 4], False)
+]
 
-def tet_class_photos_3():
-    red_shirt_heights = [1, 2, 3, 4, 5]
-    blue_shirt_heights = [2, 3, 4, 5, 6]
-    
-    assert class_photos(red_shirt_heights, blue_shirt_heights) == False
+ids = ['red_shirts: {}, blue_shirt: {}, photo_possible: {}'.format(case[0], case[1], case[2]) for case in test_cases]
 
-def tet_class_photos_4():
-    red_shirt_heights = [1, 1, 1, 1, 1, 1, 1, 1]
-    blue_shirt_heights = [2, 2, 2, 2, 2, 2, 2, 2]
+@pytest.mark.parametrize('red_shirt_heights, blue_shirt_heights, photo_possible', test_cases, ids=ids)
+def test_class_photos(red_shirt_heights, blue_shirt_heights, photo_possible):
     
-    assert class_photos(red_shirt_heights, blue_shirt_heights) == True
-
-def tet_class_photos_5():
-    red_shirt_heights = [19, 2, 4, 6, 2, 3, 1, 1, 4]
-    blue_shirt_heights = [21, 5, 4, 4, 4, 4, 4, 4, 4]
-    
-    assert class_photos(red_shirt_heights, blue_shirt_heights) == False
+    assert class_photos(red_shirt_heights, blue_shirt_heights) == photo_possible
