05b04f73b3ae0a3de44def724b0ab0f257394fa2
Kamil <kulikkamil@gmail.com>
2021-08-09 07:16:37 +0200

    add min_waiting_time tests

---
+++ tests/min_waiting_time_test.py
@@ -0,0 +1,16 @@
+import pytest
+from src.min_waiting_time.min_waiting_time import min_waiting_time
+
+test_cases = [
+    ([1, 1, 1, 4, 5, 6, 8, 1, 1, 2, 1], 81),
+    ([17, 4, 3], 10),
+    ([2], 0),
+    ([4, 3, 1, 1, 3, 2, 1, 8], 45),
+    ([1, 9], 1)
+]
+
+ids = ['queue: {}, min_wait_time: {}'.format(case[0], case[1]) for case in test_cases]
+
+@pytest.mark.parametrize('queue, min_wait_time', test_cases, ids = ids)
+def test_min_waiting_time(queue, min_wait_time):
+    assert min_waiting_time(queue) == min_wait_time
