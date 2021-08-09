4a453027f88a0bc70207c4ba6e1eee044df5c454
Kamil <kulikkamil@gmail.com>
2021-08-09 08:25:41 +0200

    add tests for min_waiting_time_smart

--- tests/min_waiting_time_test.py
+++ tests/min_waiting_time_test.py
@@ -1,5 +1,5 @@
 import pytest
-from src.min_waiting_time.min_waiting_time import min_waiting_time
+from src.min_waiting_time.min_waiting_time import min_waiting_time, min_waiting_time_smart
 
 test_cases = [
     ([1, 1, 1, 4, 5, 6, 8, 1, 1, 2, 1], 81),
@@ -9,8 +9,12 @@ test_cases = [
     ([1, 9], 1)
 ]
 
-ids = ['queue: {}, min_wait_time: {}'.format(case[0], case[1]) for case in test_cases]
+ids = ['queries: {}, min_wait_time: {}'.format(case[0], case[1]) for case in test_cases]
 
-@pytest.mark.parametrize('queue, min_wait_time', test_cases, ids = ids)
-def test_min_waiting_time(queue, min_wait_time):
-    assert min_waiting_time(queue) == min_wait_time
+@pytest.mark.parametrize('queries, min_wait_time', test_cases, ids = ids)
+def test_min_waiting_time(queries, min_wait_time):
+    assert min_waiting_time(queries) == min_wait_time
+
+@pytest.mark.parametrize('queries, min_wait_time', test_cases, ids = ids)
+def test_min_waiting_time_smart(queries, min_wait_time):
+    assert min_waiting_time_smart(queries) == min_wait_time
