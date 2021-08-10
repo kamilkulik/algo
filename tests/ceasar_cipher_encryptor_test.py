c71488b5cd0fbcce0aebea5dca01bed0b5628416
Kamil <kulikkamil@gmail.com>
2021-08-10 08:52:59 +0200

    add TDD driven ceasar_cipher_encryptor tests

---
+++ tests/ceasar_cipher_encryptor_test.py
@@ -0,0 +1,16 @@
+import pytest
+from src.ceasar_cipher_encryptor.ceasar_cipher_encryptor import ceasar_cipher_encryptor
+
+test_cases = [
+    ("kjwmntauvjjnmsagwgawkagfuaugjhawgnawgjhawjgawbfawghesh", 15, "zylbcipjkyycbhpvlvplzpvujpjvywplvcplvywplyvplquplvwthw"),
+    ("mvklahvjcnbwqvtutmfafkwiuagjkzmzwgf", 7, "zylbcipjkyycbhpvlvplzpvujpjvywplvcplvywplyvplquplvwthw"),
+    ("ovmqkwtujqmfkao", 52, "ovmqkwtujqmfkao"),
+    ("xyz", 25, "wxy"),
+    ("abc", 0, "abc")
+]
+
+ids = ['string: {}, key: {}'.format(case[0], case[1]) for case in test_cases]
+
+@pytest.mark.parametrize('string, key, output', test_cases, ids = ids)
+def test_ceasar_cipher_encryptor(string, key, output):
+    assert ceasar_cipher_encryptor(string, key) == output
