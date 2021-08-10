ba40bcf81f2db164fccef0a625bb24c5c532253a
Kamil <kulikkamil@gmail.com>
2021-08-10 10:00:35 +0200

    add tests for alternative ceasar_cipher_encryptor

--- tests/ceasar_cipher_encryptor_test.py
+++ tests/ceasar_cipher_encryptor_test.py
@@ -1,9 +1,9 @@
 import pytest
-from src.ceasar_cipher_encryptor.ceasar_cipher_encryptor import ceasar_cipher_encryptor
+from src.ceasar_cipher_encryptor.ceasar_cipher_encryptor import ceasar_cipher_encryptor, ceasar_cipher_encryptor_alt
 
 test_cases = [
     ("kjwmntauvjjnmsagwgawkagfuaugjhawgnawgjhawjgawbfawghesh", 15, "zylbcipjkyycbhpvlvplzpvujpjvywplvcplvywplyvplquplvwthw"),
-    ("mvklahvjcnbwqvtutmfafkwiuagjkzmzwgf", 7, "zylbcipjkyycbhpvlvplzpvujpjvywplvcplvywplyvplquplvwthw"),
+    ("mvklahvjcnbwqvtutmfafkwiuagjkzmzwgf", 7, "tcrshocqjuidxcabatmhmrdpbhnqrgtgdnm"),
     ("ovmqkwtujqmfkao", 52, "ovmqkwtujqmfkao"),
     ("xyz", 25, "wxy"),
     ("abc", 0, "abc")
@@ -14,3 +14,7 @@ ids = ['string: {}, key: {}'.format(case[0], case[1]) for case in test_cases]
 @pytest.mark.parametrize('string, key, output', test_cases, ids = ids)
 def test_ceasar_cipher_encryptor(string, key, output):
     assert ceasar_cipher_encryptor(string, key) == output
+    
+@pytest.mark.parametrize('string, key, output', test_cases, ids = ids)
+def test_ceasar_cipher_encryptor_alt(string, key, output):
+    assert ceasar_cipher_encryptor_alt(string, key) == output
