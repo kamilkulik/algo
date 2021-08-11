ccc681ec27f632512c512fbb5e962f3e6a6423eb
Kamil <kulikkamil@gmail.com>
2021-08-11 06:35:11 +0200

    refactor validate_subsequence algo tests

--- tests/validate_subsequence_test.py
+++ tests/validate_subsequence_test.py
@@ -1,33 +1,20 @@
-from src.validate_subsequence.validate_subsequence import validate_subsequence
-from src.validate_subsequence.validate_subsequence import validate_subquence_for_loop
+import pytest
+from src.validate_subsequence.validate_subsequence import validate_subsequence, validate_subsequence_for_loop
 
-def test_validate_subsequence_1():
-    assert validate_subsequence([1, 2, 3, 4], [2, 3]) == True
+test_cases = [
+    ([1, 2, 3, 4], [2, 3], True),
+    ([5, 1, 22, 25, 6, -1, 8, 10], [22, 25, 6], True),
+    ([1, 1, 1, 1, 1], [1, 1, 1], True),
+    ([5, 1, 22, 25, 6, -1, 8, 10], [5, 1, 22, 25, 6, -1, 8, 10, 12], False),
+    ([5, 1, 22, 25, 6, -1, 8, 10], [1, 6, -1, 10, 11, 11, 11, 11], False)
+]
 
-def test_validate_subsequence_2():
-    assert validate_subsequence([5, 1, 22, 25, 6, -1, 8, 10], [22, 25, 6]) == True
+ids = ["array: {}, subarray: {}".format(case[0], case [1]) for case in test_cases]
 
-def test_validate_subsequence_3():
-    assert validate_subsequence([1, 1, 1, 1, 1], [1, 1, 1]) == True
+@pytest.mark.parametrize('array, subarray, result', test_cases, ids = ids)
+def test_validate_subsequence(array, subarray, result):
+    assert validate_subsequence(array, subarray) == result
 
-def test_validate_subsequence_4():
-    assert validate_subsequence([5, 1, 22, 25, 6, -1, 8, 10], [5, 1, 22, 25, 6, -1, 8, 10, 12]) == False
-
-def test_validate_subsequence_5():
-    assert validate_subsequence([5, 1, 22, 25, 6, -1, 8, 10], [1, 6, -1, 10, 11, 11, 11, 11]) == False
-
-def test_validate_subquence_for_loop_1():
-    assert validate_subquence_for_loop([1, 2, 3, 4], [2, 3]) == True
-
-def test_validate_subquence_for_loop_2():
-    assert validate_subquence_for_loop([5, 1, 22, 25, 6, -1, 8, 10], [22, 25, 6]) == True
-
-def test_validate_subquence_for_loop_3():
-    assert validate_subquence_for_loop([1, 1, 1, 1, 1], [1, 1, 1]) == True
-
-def test_validate_subquence_for_loop_4():
-    assert validate_subquence_for_loop([5, 1, 22, 25, 6, -1, 8, 10], [5, 1, 22, 25, 6, -1, 8, 10, 12]) == False
-
-def test_validate_subquence_for_loop_5():
-    assert validate_subquence_for_loop([5, 1, 22, 25, 6, -1, 8, 10], [1, 6, -1, 10, 11, 11, 11, 11]) == False
-
+@pytest.mark.parametrize('array, subarray, result', test_cases, ids = ids)
+def test_validate_subsequence_for_loop(array, subarray, result):
+    assert validate_subsequence_for_loop(array, subarray) == result
