1b2ffa8c160e2dba76b93073615573e6398a4767
Kamil <kulikkamil@gmail.com>
2021-08-11 07:13:44 +0200

    refactor tests for sorted_squared_array algo

--- tests/sorted_squared_array_test.py
+++ tests/sorted_squared_array_test.py
@@ -1,42 +1,22 @@
-from src.sorted_squared_array.sorted_squared_array import sorted_squared_array_naive
-from src.sorted_squared_array.sorted_squared_array import sorted_squared_array
-from src.sorted_squared_array.sorted_squared_array import sorted_squared_array_no_reversal
+import pytest
+from src.sorted_squared_array.sorted_squared_array import sorted_squared_array_naive, sorted_squared_array, sorted_squared_array_no_reversal
 
 test_cases = [
-    {
-        "inputs": [[-50, -13, -2, -1, 0, 0, 1, 1, 2, 3, 19, 20]],
-        "expected_result": [0, 0, 1, 1, 1, 4, 4, 9, 169, 361, 400, 2500]
-    },
-    {
-        "inputs": [[0]],
-        "expected_result": [0]
-    },
-    {
-        "inputs": [[1, 2, 3, 5, 6, 8, 9]],
-        "expected_result": [1, 4, 9, 25, 36, 64, 81]
-    },
+    ([-50, -13, -2, -1, 0, 0, 1, 1, 2, 3, 19, 20],[0, 0, 1, 1, 1, 4, 4, 9, 169, 361, 400, 2500]),
+    ([0],[0]),
+    ([1, 2, 3, 5, 6, 8, 9],[1, 4, 9, 25, 36, 64, 81]),
 ]
 
-functions = [
-    sorted_squared_array_naive,
-    sorted_squared_array,
-    sorted_squared_array_no_reversal,
-]
+ids = ["array: {}".format(case[0]) for case in test_cases]
 
-def test_sorted_squared_array_naive():
-    for test_case in test_cases:
-        inputs = test_case["inputs"]
-        result = test_case["expected_result"]
-        assert sorted_squared_array_naive(*inputs) == result
+@pytest.mark.parametrize('array, sorted_squares', test_cases, ids = ids)
+def test_sorted_squared_array_naive(array, sorted_squares):
+    assert sorted_squared_array_naive(array) == sorted_squares
 
-def test_sorted_squared_array():
-    for test_case in test_cases:
-        inputs = test_case["inputs"]
-        result = test_case["expected_result"]
-        assert sorted_squared_array(*inputs) == result
+@pytest.mark.parametrize('array, sorted_squares', test_cases, ids = ids)
+def test_sorted_squared_array(array, sorted_squares):
+    assert sorted_squared_array(array) == sorted_squares
 
-def test_sorted_squared_array_no_reversal():
-    for test_case in test_cases:
-        inputs = test_case["inputs"]
-        result = test_case["expected_result"]
-        assert sorted_squared_array_no_reversal(*inputs) == result
+@pytest.mark.parametrize('array, sorted_squares', test_cases, ids = ids)
+def test_sorted_squared_array_no_reversal(array, sorted_squares):
+    assert sorted_squared_array_no_reversal(array) == sorted_squares
