9386a891e068e01ee199f615e49f70094629e329
Kamil <kulikkamil@gmail.com>
2021-08-14 17:01:48 +0200

    refactor tests for finding the closest value in BST

--- tests/find_closest_value_in_bst_test.py
+++ tests/find_closest_value_in_bst_test.py
@@ -1,36 +1,25 @@
-from src.find_closest_value_in_bst.find_closest_value_in_bst import find_closest_value_in_bst
-from src.binary_search_tree.binary_search_tree import BST
+import pytest
+from src.find_closest_value_in_bst.find_closest_value_in_bst import find_closest_value_in_bst, find_closest_value_in_bst_recursive
 from src.binary_search_tree.build_bst import build_binary_search_tree
 
-def test_find_closest_value_in_bst_1():
-    
-    tree = {
-        "root": 10,
-        "nodes": [15, 22, 13, 14, 5, 5, 2, 1]
-    }
-    
-    binary_tree = build_binary_search_tree(tree)
+test_cases = [
+    ([15, 22, 13, 14, 5, 5, 2, 1], 10, 12, 13),
+    ([502, 55000, 1001, 4500, 204, 205, 207, 208, 206, 203, 5, 15, 22, 57, 60, 5, 2, 3, 1, 1, 1, 1, 1, -51, -403], 100, 100, 100),
+    ([502, 55000, 1001, 4500, 204, 205, 207, 208, 206, 203, 5, 15, 22, 57, 60, 5, 2, 3, 1, 1, 1, 1, 1, -51, -403], 100, -70, -51),
+]
 
-    assert find_closest_value_in_bst(binary_tree, 12) == 13
+ids = ['target: {}, closest: {}'.format(case[2], case[3]) for case in test_cases]
 
-def test_find_closest_value_in_bst_2():
+@pytest.mark.parametrize('tree, root, target, closest', test_cases, ids = ids)
+def test_find_closest_value_in_bst(tree, root, target, closest):
     
-    tree = {
-        "root": 100,
-        "nodes": [502, 55000, 1001, 4500, 204, 205, 207, 208, 206, 203, 5, 15, 22, 57, 60, 5, 2, 3, 1, 1, 1, 1, 1, -51, -403]
-    }
-    
-    binary_tree = build_binary_search_tree(tree)
+    binary_tree = build_binary_search_tree(tree, root)
 
-    assert find_closest_value_in_bst(binary_tree, 100) == 100
-    
-def test_find_closest_value_in_bst_3():
-    
-    tree = {
-        "root": 100,
-        "nodes": [502, 55000, 1001, 4500, 204, 205, 207, 208, 206, 203, 5, 15, 22, 57, 60, 5, 2, 3, 1, 1, 1, 1, 1, -51, -403]
-    }
-    
-    binary_tree = build_binary_search_tree(tree)
+    assert find_closest_value_in_bst(binary_tree, target) == closest
 
-    assert find_closest_value_in_bst(binary_tree, -70) == -51
+@pytest.mark.parametrize('tree, root, target, closest', test_cases, ids = ids)
+def test_find_closest_value_in_bst_recursive(tree, root, target, closest):
+    
+    binary_tree = build_binary_search_tree(tree, root)
+
+    assert find_closest_value_in_bst_recursive(binary_tree, target, root) == closest
