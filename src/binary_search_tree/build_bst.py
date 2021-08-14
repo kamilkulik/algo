12ddb44d8237d0e4ecb8672d0a8bfcea35d2bc0c
Kamil <kulikkamil@gmail.com>
2021-08-14 17:02:27 +0200

    refactor the function for building a binary tree

--- src/binary_search_tree/build_bst.py
+++ src/binary_search_tree/build_bst.py
@@ -1,8 +1,7 @@
 from src.binary_search_tree.binary_search_tree import BST
 
-def build_binary_search_tree(tree):
-    binary_tree = BST(tree["root"])
-    remaining_nodes = tree["nodes"]
+def build_binary_search_tree(remaining_nodes,root):
+    binary_tree = BST(root)
 
     for node in remaining_nodes:
         binary_tree.insert(node)
