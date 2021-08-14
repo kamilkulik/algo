a8e0f1503cd3cf78e4ffb2126f98954d5aaa5aea
Kamil <kulikkamil@gmail.com>
2021-08-14 17:03:05 +0200

    add semi-recursive version of the find_closest_value_in_bst algo

--- src/find_closest_value_in_bst/find_closest_value_in_bst.py
+++ src/find_closest_value_in_bst/find_closest_value_in_bst.py
@@ -14,3 +14,18 @@ def find_closest_value_in_bst(tree, target):
         else:
             break
     return best_match
+
+
+def find_closest_value_in_bst_recursive(tree, target, closest):
+    # base case is checking if the tree is none - we want that because we sum current node's value
+    if tree is None: return closest
+    # second we want to check if target minus the value of current node is less than target - closest
+    # this is a check for smaller difference between the target and any of the nodes
+    if abs(target - closest) > abs(target - tree.value): closest = tree.value
+    # next we are checking if the target is less or greater than the value of the current node
+    # since this is a BST, we can call the function recursively for both smaller and greater values
+    if target > tree.value: return find_closest_value_in_bst_recursive(tree.right, target, closest)
+    elif target < tree.value: return find_closest_value_in_bst_recursive(tree.left, target, closest)
+    # when non of the above is hit, we've arrived to the right-most node of the BST
+    # time to return
+    else: return closest
