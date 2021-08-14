b89f2e7e037fb049a7d705d6a2ac1b1d178d12fb
Kamil <kulikkamil@gmail.com>
2021-08-14 16:04:20 +0200

    refactor min_nonconstructible_change tests cases

--- tests/non_constructible_change_test.py
+++ tests/non_constructible_change_test.py
@@ -1,30 +1,16 @@
+import pytest
 from src.non_constructible_change.non_constructible_change import non_constructible_change
 
 test_cases = [
-    {
-        "coins": [109, 2000, 8765, 19, 18, 17, 16, 8, 1, 1, 2, 4],
-        "result": 87,
-    },
-    {
-        "coins": [1],
-        "result": 2,
-    },
-    {
-        "coins": [5, 6, 1, 1, 2, 3, 4, 9],
-        "result": 32,
-    },
-    {
-        "coins": [5, 6, 1, 1, 2, 3, 43],
-        "result": 19,
-    },
-    {
-        "coins": [1, 1],
-        "result": 3,
-    }
+    ([109, 2000, 8765, 19, 18, 17, 16, 8, 1, 1, 2, 4],87),
+    ([1],2),
+    ([5, 6, 1, 1, 2, 3, 4, 9],32),
+    ([5, 6, 1, 1, 2, 3, 43],19),
+    ([1, 1],3),
 ]
 
-def test_non_constructible_change():
-    for test_case in test_cases:
-        coins = test_case["coins"]
-        result = test_case["result"]
-        assert non_constructible_change(coins) == result
+ids = ["Coins: {}".format(case[0]) for case in test_cases]
+
+@pytest.mark.parametrize('coins, result', test_cases, ids = ids)
+def test_non_constructible_change(coins, result):
+    assert non_constructible_change(coins) == result
