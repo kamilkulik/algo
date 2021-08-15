55e3bb3afd7828e03cf2125968b59cc799ce521c
Kamil <kulikkamil@gmail.com>
2021-08-15 22:33:30 +0200

    add explainatory comments to the depth_first_search algorithm

--- src/depth_first_search/depth_first_search.py
+++ src/depth_first_search/depth_first_search.py
@@ -11,7 +11,13 @@ class Node():
     # Time: O(V + E) where: V are vertexes of the graph and E are its edges
     # Space: O(V) meaning the array of results will have length equal to number of vertexes
     def depth_first_search(self, array):
+        # 1. add idea of the root node to the array
         array.append(self.id)
+        # 2. loop over node's children
         for child in self.children:
+            # 3. call child's depth_first_search method because
+            # if we called it on self then it would get into an infinite loop
+            # since we'd be always iterating over the same children
             child.depth_first_search(array)
+            # 4. return the array
         return array
