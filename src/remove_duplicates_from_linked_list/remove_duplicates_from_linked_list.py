93cfa74e57d9caebc190cc20005840523c19afb8
Kamil <kulikkamil@gmail.com>
2021-08-16 08:47:43 +0200

    add precedural comments to the remove_dupplicates_from_linked_list algo

--- src/remove_duplicates_from_linked_list/remove_duplicates_from_linked_list.py
+++ src/remove_duplicates_from_linked_list/remove_duplicates_from_linked_list.py
@@ -27,15 +27,23 @@ def removeDuplicatesFromLinkedList_separately(linkedList):
 # O(n) time and O(1) space
 
 def removeDuplicatesFromLinkedList(linked_list):
+    # 1. We need to keep track of the first unique node - be default it will be the first node we pass into the function
     current_node = linked_list
-	
+	# 2. We then use a while loop which will exit when current_node is None
     while current_node is not None:
+        # 3. We use a look ahead approach. Let's initiate a variable to hold our next_unique_value.
+        # the plan is to check each value if it's unique compared to current_node's value
+        # and move on to the next node in case of value duplication
         next_unique_node = current_node.next
-        
+        # 4. We do a second while loop which will exit only when the next node has unique value
         while next_unique_node is not None and current_node.value == next_unique_node.value:
+            # 5. Assign next_unique_node to the next node (because the while loop fired which means a duplicate was found)
             next_unique_node = next_unique_node.next
-        
+        # 6. outside of second while loop, reassign current_node's next value to the next_unique node
+        # if the subsequent node was unique, we'll reassign the same node
+        # if node wasn't unique we'll assign the next unique node
         current_node.next = next_unique_node
+        # 7. now we need to reassign the current node so the while loop can continue
         current_node = next_unique_node
         
     return linked_list
