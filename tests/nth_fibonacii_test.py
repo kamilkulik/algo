55d43af80230f5195ee50b00bb4c2fe052493169
Kamil <kulikkamil@gmail.com>
2021-08-21 08:32:25 +0200

    nth-fib recusrsive with memoisation tests

--- tests/nth_fibonacii_test.py
+++ tests/nth_fibonacii_test.py
@@ -17,3 +17,7 @@ def test_nth_fib_naive(number, nth_fib):
 @pytest.mark.parametrize('number, nth_fib', test_cases)
 def test_nth_fib(number, nth_fib):
     assert nthFib(number) == nth_fib
+
+@pytest.mark.parametrize('number, nth_fib', test_cases)
+def test_nthFib_recursion_memoised(number, nth_fib):
+    assert nthFib_recursion_memoised(number) == nth_fib
