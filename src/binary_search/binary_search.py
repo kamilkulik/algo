1c055dc7c64589912a048fd783395f0ae4ad2ac0
kamilkulikTeck <kulikkamil@gmail.com>
2021-08-22 22:13:42 +0200

    add comment to the binary_search algo

--- src/binary_search/binary_search.py
+++ src/binary_search/binary_search.py
@@ -2,24 +2,25 @@
 # the iterative approach
 
 def binarySearch(array, target):
-	# initilise two pointers - left and right to keep track of current bounds
-	left = 0
-	right = len(array) - 1
+	# 1. Pass into helper function two pointers - left and right to keep track of current bounds
+	return binary_search_helper(array, target, left = 0, right = len(array) - 1)
 
+def binary_search_helper(array, target, left, right):
+	# 2. Loop over the array while left pointer is less or equal right pointer
 	while left <= right:
-		middle = (right + left) // 2 # determine the index of middle number in the array
-
+		# 3. Determine the index of middle number in the array
+		middle = (right + left) // 2 # // returns quotient as integer (rounded down)
+		# 4. Check if middle of array is equal to the target value
 		if array[middle] == target:
 			return middle
+		# 5. if determined middle is greater than target, we need to look at the array to the left of the middle (excluding it)
 		elif array[middle] > target:
 			# we move the right pointer to the left excluding the middle index
-			right = middle - 1
+			right = middle - 1 # -1 is used to exlcude the element at the middle pointer
+		# 6. do the opposite to point 5
 		elif array[middle] < target:
 			left = middle + 1
 	return -1 # in case such number wasn't found the array
-
-print(binarySearch([1, 4, 5, 7, 8, 10, 13, 16, 19, 20, 26, 43, 48, 49, 50, 53, 55, 59, 64, 67, 80], 26))
-print(binarySearch([1, 4, 5, 7, 8, 10, 13, 16, 19, 20, 26, 43, 48, 49, 50, 53, 55, 59, 64, 67, 80], 68))
 
 # RECURSIVE SOLUTION
 
@@ -43,6 +44,3 @@ def binarySearchRecursiveHelper(array, target, left, right):
 	elif array[middle] < target:
 		left = middle + 1
 		return binarySearchRecursiveHelper(array, target, left, right)
-
-print(binarySearchRecursive([1, 4, 5, 7, 8, 10, 13, 16, 19, 20, 26, 43, 48, 49, 50, 53, 55, 59, 64, 67, 80], 26))
-print(binarySearchRecursive([1, 4, 5, 7, 8, 10, 13, 16, 19, 20, 26, 43, 48, 49, 50, 53, 55, 59, 64, 67, 80], 68))
