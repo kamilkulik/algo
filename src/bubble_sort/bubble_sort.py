05c077f49f5072823fcb3eb8ec21d41c055c759c
kamilkulikTeck <kulikkamil@gmail.com>
2021-08-24 08:52:58 +0200

    slight refactor of the bubble sort algorithm

--- src/bubble_sort/bubble_sort.py
+++ src/bubble_sort/bubble_sort.py
@@ -1,12 +1,14 @@
 # the idea is to iterate over an array
-# at each iteration compare two adjacent numbers are put them in ascending order if necessary
+# at each iteration compare two adjacent numbers and put them in ascending order if necessary
 # algorithm will have completed after no sorting was done in last iteration
 
-# O(n^2) time and O(1) space
+# Worst case complexity: O(n^2) time and O(1) space
+# Average case complexity: O(n^2) time and O(1) space
+# Best case complexity: O(n) time and O(1) space - when the array is sorted
 
 def bubble_sort(array):
     is_sorted = False
-    while is_sorted is not True:
+    while not is_sorted:
         is_sorted = True
         for i in range(len(array) - 1):
             if array[i] > array[i + 1]:
@@ -24,13 +26,13 @@ print(bubble_sort([5, 2, 1, -6, 10, 123, -432, 78, 44]))
 def bubble_sort_optimised(array):
     is_sorted = False
     counter = 0 # introduce a counter which will increase by 1 at each iteration
-    while is_sorted is not True:
+    while not is_sorted:
         is_sorted = True
         for i in range(len(array) - 1 - counter): # because at each iteration the maximum number will be pushed to the end of array we don't have to iterate over it in the next iteration
             if array[i] > array[i + 1]:
                 swap(array, i)
                 is_sorted = False
-        counter += 1 # increment at the end of each full iteration (outside while loop)
+        counter += 1 # increment at the end of each full iteration (outside for loop)
     return array
 
 def swap(array, i):
