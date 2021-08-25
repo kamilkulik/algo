5274256449612bf7e040ae6f4b843c3d62bb8e94
kamilkulikTeck <kulikkamil@gmail.com>
2021-08-25 08:50:13 +0200

    add comments to the selection_sort algo

--- src/selection_sort/selection_sort.py
+++ src/selection_sort/selection_sort.py
@@ -8,13 +8,23 @@
 # O(n^2) time and O(1) space
 
 def selection_sort(array):
+    # 1. we need current idx because we'll always be looking forward
     current_idx = 0
+    # 2. initiate while loop which will exit after we have looked at the penultimate element
     while current_idx < len(array) -1:
+        # 3. initialise new variable declared as smallest_idx - we'll use it to track the index of smallest element
         smallest_idx = current_idx
+        # 4. Look ahead loop. For loop that starts at the element after current_idx and loop over to the end of array
         for i in range(current_idx + 1, len(array)):
+            # 5. check if the current element is smaller than the next one
             if array[smallest_idx] > array[i]:
+                # 6. If not, reassign smallest_idx to i
                 smallest_idx = i
+                # 7. The for loop repeats this for the rest of the array
+                # Finally, we'll have the index of the smallest elements in current tick on the while loop
+        # 8. swap the element from current idx (starting at first position, and moving incrementally to the end of array) with the smallest element
         swap(current_idx, smallest_idx, array)
+        # 9. increment current_idx for the next tick of the while loop
         current_idx += 1
     return array
 
