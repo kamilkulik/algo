44a41308ef9f49eead988c193eab9278fe575554
Kamil <kulikkamil@gmail.com>
2021-09-08 07:43:46 +0200

    add overview of the smallest_difference algo

---
+++ src/smallest_difference/smallest_difference.py
@@ -0,0 +1,5 @@
+# the idea is to use two pointers
+# left and right representing first and last element of the array
+# we will sort the two input arrays and use that to determine which pointer needs to move at a given time
+
+def smallest_difference(first_array, second_array):
