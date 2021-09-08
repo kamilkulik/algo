af41a5c4cb78960dc941f26884e363e0c64580db
kamilkulikTeck <kulikkamil@gmail.com>
2021-09-08 07:14:45 +0200

    get rid of print in insertion sort

--- src/insertion_sort/insertion_sort.py
+++ src/insertion_sort/insertion_sort.py
@@ -9,7 +9,7 @@
 # O(n^2) time and O(1) space
 
 def insertion_sort(array):
-    for i in range(1, len(array)):
+    for i in range(len(array)):
         j = i
         while j > 0 and array[j] < array[j - 1]:
             array[j -1], array [j] = array[j], array[j -1]
