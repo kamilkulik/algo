81a6053bfb5e52c10216cb3915a938eda22537e7
Kamil <kulikkamil@gmail.com>
2021-09-09 06:44:10 +0200

    add TDD driven tests for the move_element_to_end algo

---
+++ tests/move_element_to_end_test.py
@@ -0,0 +1,17 @@
+import pytest
+from src.move_element_to_end.move_element_to_end import move_element_to_end
+
+test_cases = [
+    ([2, 1, 2, 2, 2, 3, 4, 2], 2, [4, 1, 3, 2, 2, 2, 2, 2]),
+    ([], 3, []),
+    ([1, 2, 4, 5, 6], 3, [1, 2, 4, 5, 6]),
+    ([3, 3, 3, 3, 3], 3, [3, 3, 3, 3, 3]),
+    ([3, 1, 2, 4, 5], 3, [5, 1, 2, 4, 3]),
+    ([5, 1, 2, 5, 5, 3, 4, 6, 7, 5, 8, 9, 10, 11, 5, 5, 12], 5, [12, 1, 2, 11, 10, 3, 4, 6, 7, 9, 8, 5, 5, 5, 5, 5, 5])
+]
+
+ids = ["array: {}, target: {}".format(case[0], case[1]) for case in test_cases]
+
+@pytest.mark.parametrize('input, target, result', test_cases, ids = ids)
+def test_move_element_to_end(input, target, result):
+    assert move_element_to_end(input, target) == result
