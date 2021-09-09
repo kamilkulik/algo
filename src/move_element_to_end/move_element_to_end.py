2633a57209ea0ac474267e1427a2bf4013f741d9
Kamil <kulikkamil@gmail.com>
2021-09-09 06:59:24 +0200

    complete move_element_to_end algo

--- src/move_element_to_end/move_element_to_end.py
+++ src/move_element_to_end/move_element_to_end.py
@@ -1,2 +1,16 @@
-def move_element_to_end(array, target):
-    pass
+def move_element_to_end(array, to_move):
+    left_pointer = 0
+    right_pointer = len(array) - 1
+
+    while left_pointer < right_pointer:
+
+        while array[right_pointer] == to_move and left_pointer < right_pointer:
+
+            right_pointer -= 1
+
+        if array[left_pointer] == to_move:
+            array[left_pointer], array[right_pointer] = array[right_pointer], array[left_pointer]
+
+        left_pointer += 1
+
+    return array
