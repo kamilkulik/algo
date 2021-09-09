f5e008f78f885b21b153c210f73033ab60c01cc1
Kamil <kulikkamil@gmail.com>
2021-09-09 07:00:54 +0200

    update tests for move_element_to_end_test

--- tests/move_element_to_end_test.py
+++ tests/move_element_to_end_test.py
@@ -10,8 +10,8 @@ test_cases = [
     ([5, 1, 2, 5, 5, 3, 4, 6, 7, 5, 8, 9, 10, 11, 5, 5, 12], 5, [12, 1, 2, 11, 10, 3, 4, 6, 7, 9, 8, 5, 5, 5, 5, 5, 5])
 ]
 
-ids = ["array: {}, target: {}".format(case[0], case[1]) for case in test_cases]
+ids = ["array: {}, to_move: {}".format(case[0], case[1]) for case in test_cases]
 
-@pytest.mark.parametrize('input, target, result', test_cases, ids = ids)
-def test_move_element_to_end(input, target, result):
-    assert move_element_to_end(input, target) == result
+@pytest.mark.parametrize('input, to_move, result', test_cases, ids = ids)
+def test_move_element_to_end(input, to_move, result):
+    assert move_element_to_end(input, to_move) == result
