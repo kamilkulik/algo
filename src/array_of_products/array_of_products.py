47b1efc2e9aa885a2aa43e90ec82645e79c484ba
Kamil <kulikkamil@gmail.com>
2021-09-20 10:22:59 +0200

    add array_of_products brute and optimised algos

---
+++ src/array_of_products/array_of_products.py
@@ -0,0 +1,31 @@
+# BRUTE FORCE
+
+def array_of_products_brute(array):
+    products = []
+    for i in range(len(array)):
+        running_product = 1
+        for j in range(len(array)):
+            if i != j:
+                running_product *= array[j]
+        products.append(running_product)
+    return products
+
+def array_of_products(array):
+    products = [1 for _ in range(len(array))]
+    left_array = [1 for _ in range(len(array))]
+    right_array = [1 for _ in range(len(array))]
+
+    running_product_left = 1
+    for i in range(len(array)):
+        left_array[i] = running_product_left
+        running_product_left *= array[i]
+
+    running_product_right = 1
+    for i in reversed(range(len(array))):
+        right_array[i] = running_product_right
+        running_product_right *= array[i]
+
+    for i in range(len(array)):
+        products[i] = left_array[i] * right_array[i]
+
+    return products
--- tests/array_of_products_test.py
+++ tests/array_of_products_test.py
@@ -1,10 +1,10 @@
 import pytest
-from src.array_of_products.array_of_products import array_of_products
+from src.array_of_products.array_of_products import array_of_products, array_of_products_brute
 
 test_cases = [
     ([0, 0, 0, 0], [0, 0, 0, 0]),
     ([1, 1, 1, 1], [1, 1, 1, 1]),
-    ([-1, -1, -1], [-1, -1, -1]),
+    ([-1, -1, -1], [1, 1, 1]),
     ([0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
     ([1, 8, 6, 2, 4], [384, 48, 64, 192, 96]),
     ([-5, 2, -4, 14, -6], [672, -1680, 840, -240, 560])
@@ -12,6 +12,10 @@ test_cases = [
 
 ids = ['input array: {}'.format(case[0]) for case in test_cases]
 
+
+@pytest.mark.parametrize('input_array, output_array', test_cases, ids=ids)
+def test_array_of_products_brute(input_array, output_array):
+    assert array_of_products_brute(input_array) == output_array
 
 @pytest.mark.parametrize('input_array, output_array', test_cases, ids=ids)
 def test_array_of_products(input_array, output_array):
