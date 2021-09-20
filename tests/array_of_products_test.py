fcbc56a61db07750dad22d3ded28649b8aa8cd94
Kamil <kulikkamil@gmail.com>
2021-09-20 09:23:22 +0200

    TDD driven tests for the array_of_products algo

---
+++ tests/array_of_products_test.py
@@ -0,0 +1,18 @@
+import pytest
+from src.array_of_products.array_of_products import array_of_products
+
+test_cases = [
+    ([0, 0, 0, 0], [0, 0, 0, 0]),
+    ([1, 1, 1, 1], [1, 1, 1, 1]),
+    ([-1, -1, -1], [-1, -1, -1]),
+    ([0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
+    ([1, 8, 6, 2, 4], [384, 48, 64, 192, 96]),
+    ([-5, 2, -4, 14, -6], [672, -1680, 840, -240, 560])
+]
+
+ids = ['input array: {}'.format(case[0]) for case in test_cases]
+
+
+@pytest.mark.parametrize('input_array, output_array', test_cases, ids=ids)
+def test_array_of_products(input_array, output_array):
+    assert array_of_products(input_array) == output_array
