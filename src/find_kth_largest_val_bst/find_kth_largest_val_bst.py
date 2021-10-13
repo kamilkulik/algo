992d066abf6f04a525613c89e60c30bb7413d53d
Kamil <kulikkamil@gmail.com>
2021-10-13 08:18:51 +0200

    add the find_kth_largest_val_bst algo

---
+++ src/find_kth_largest_val_bst/find_kth_largest_val_bst.py
@@ -0,0 +1,20 @@
+class TraversalInfo:
+    def __init__(self, nodes_visited, kth_value):
+        self.nodes_visited = nodes_visited
+        self.kth_value = kth_value
+
+
+def find_kth_largest_val_bst(bst, k):
+    traversal_info = TraversalInfo(0, -1)
+    reverse_in_order_traverse(bst, k, traversal_info)
+    return traversal_info.kth_value
+
+
+def reverse_in_order_traverse(node, k, info):
+    if node is None or info.nodes_visited >= k:
+        return
+    reverse_in_order_traverse(node.right, k, info)
+    if info.nodes_visited < k:
+        info.nodes_visited += 1
+        info.kth_value = node.value
+        reverse_in_order_traverse(node.left, k, info)
