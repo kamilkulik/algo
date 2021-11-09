612b6ec27fb8d07cb126896d964d7730cf36ca89
Kamil <kulikkamil@gmail.com>
2021-11-09 10:18:30 +0100

    adjust the bubble_sort algo

--- src/bubble_sort/bubble_sort.py
+++ src/bubble_sort/bubble_sort.py
@@ -6,6 +6,7 @@
 # Average case complexity: O(n^2) time and O(1) space
 # Best case complexity: O(n) time and O(1) space - when the array is sorted
 
+
 def bubble_sort(array):
     is_sorted = False
     while not is_sorted:
@@ -16,22 +17,24 @@ def bubble_sort(array):
                 is_sorted = False
     return array
 
+
 def swap(array, i):
     array[i], array[i + 1] = array[i + 1], array[i]
+
 
 # slight optimisation that doesn't affect space-time complexity:
 
+
 def bubble_sort_optimised(array):
     is_sorted = False
-    counter = 0 # introduce a counter which will increase by 1 at each iteration
+    counter = 0  # introduce a counter which will increase by 1 at each iteration
     while not is_sorted:
         is_sorted = True
-        for i in range(len(array) - 1 - counter): # because at each iteration the maximum number will be pushed to the end of array we don't have to iterate over it in the next iteration
+        for i in range(
+            len(array) - 1 - counter
+        ):  # because at each iteration the maximum number will be pushed to the end of array we don't have to iterate over it in the next iteration
             if array[i] > array[i + 1]:
                 swap(array, i)
                 is_sorted = False
-        counter += 1 # increment at the end of each full iteration (outside for loop)
+        counter += 1  # increment at the end of each full iteration (outside for loop)
     return array
-
-def swap(array, i):
-    array[i], array[i + 1] = array[i + 1], array[i]
