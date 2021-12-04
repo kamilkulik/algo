a4f4ee870389042ba723e2a44be3ff970020e114
Kamil <kulikkamil@gmail.com>
2021-12-04 16:28:30 +0100

    [remove-kth-from-end] update the algo

--- src/remove_kth_from_end/remove_kth_from_end.py
+++ src/remove_kth_from_end/remove_kth_from_end.py
@@ -6,16 +6,18 @@ def remove_kth_from_end(head, k):
         second = second.next
         counter += 1
     # head node
-    if first == head:
-        head.value = first.next.value
-        head.next = first.next.next
+    if second is None:
+        head.value = head.next.value
+        head.next = head.next.next
+        return
     # other nodes
     # first move both pointers till the end
     # second will become None
-    while second is not None:
+    while second.next is not None:
         first = first.next
         second = second.next
     # first should be point now to the node
     # BEFORE the k-th node from end
     # which is the node for removal
     first.next = first.next.next
+    return head
