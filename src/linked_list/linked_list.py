31efdd75a132f42da4a142fe0311d976a146621d
Kamil <kulikkamil@gmail.com>
2021-12-04 15:19:30 +0100

    [remove-kth-from-end] add build_linked_list function

--- src/linked_list/linked_list.py
+++ src/linked_list/linked_list.py
@@ -2,7 +2,18 @@ class Linked_list:
     def __init__(self, value):
         self.value = value
         self.next = None
-    
+
     def insert(self, value):
         next_node = Linked_list(value)
         self.next = next_node
+
+
+def build_linked_list(input):
+    nodes = input["linkedList"]["nodes"]
+    linked_list = None
+    for node in nodes:
+        if linked_list is None:
+            linked_list = Linked_list(node["value"])
+            continue
+        linked_list.insert(node["value"])
+    return linked_list
