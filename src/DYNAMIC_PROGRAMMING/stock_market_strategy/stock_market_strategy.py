40350a36a88a832fb1702382702cd97b1a95904e
Kamil <kulikkamil@gmail.com>
2021-12-31 08:15:14 +0100

    finalise the stock_market_strategy algo

--- src/DYNAMIC_PROGRAMMING/stock_market_strategy/stock_market_strategy.py
+++ src/DYNAMIC_PROGRAMMING/stock_market_strategy/stock_market_strategy.py
@@ -1,7 +1,26 @@
-def dp_td_recursive(trading_days):
-    def optimal_profit(trading_day, stock_owned):
-        if trading_day == len(trading_days):
+from functools import lru_cache
+
+
+def dp_td_recursive(daily_price):
+    @lru_cache(maxsize=None)
+    def optimal_profit(trading_day, have_stock):
+        if trading_day == len(daily_price):
             return 0
-        if trading_day == 0:
-            if stock_owned > 0:
+        if trading_day < 0:
+            if not have_stock:
+                return 0
+            else:
                 return -float("inf")
+        price = daily_price[trading_day]
+        if have_stock:
+            hold_strategy = optimal_profit(trading_day - 1, True)
+            buy_strategy = optimal_profit(trading_day - 1, False) - price
+            return max(hold_strategy, buy_strategy)
+        else:
+            sell_strategy = optimal_profit(trading_day - 1, True) + price
+            avoid_strategy = optimal_profit(trading_day - 1, False)
+            return max(sell_strategy, avoid_strategy)
+
+    last_day = len(daily_price) - 1
+    no_stock = False
+    return optimal_profit(last_day, no_stock)
