50d2104212aa33d8581d91715e3a17365eebb93d
Kamil <kulikkamil@gmail.com>
2021-12-31 11:59:41 +0100

    add test for dp_bu_iterative algo

--- tests/DYNAMIC_PROGRAMMING/stock_market_strategy/stock_market_strategy_test.py
+++ tests/DYNAMIC_PROGRAMMING/stock_market_strategy/stock_market_strategy_test.py
@@ -1,5 +1,5 @@
 import pytest
-from src.DYNAMIC_PROGRAMMING.stock_market_strategy.stock_market_strategy import dp_td_recursive
+from src.DYNAMIC_PROGRAMMING.stock_market_strategy.stock_market_strategy import dp_td_recursive, dp_bu_iterative
 
 test_cases = [([2, 5, 1], 3), ([2, 5, 1, 3], 5)]
 
@@ -9,3 +9,8 @@ ids = ["Trading days: {}, Profit: {}".format(test_case[0], test_case[1]) for tes
 @pytest.mark.parametrize("trading_days, profit", test_cases, ids=ids)
 def test_stock_market_strategy_dp_td(trading_days, profit):
     assert dp_td_recursive(trading_days) == profit
+
+
+@pytest.mark.parametrize("trading_days, profit", test_cases, ids=ids)
+def test_stock_market_strategy_dp_bu(trading_days, profit):
+    assert dp_bu_iterative(trading_days) == profit
