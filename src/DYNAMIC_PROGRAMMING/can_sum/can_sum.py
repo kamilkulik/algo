b0cbf63e14fdad730a6fc6b2fdd678ca654d2c97
Kamil <kulikkamil@gmail.com>
2022-01-04 20:10:26 +0100

    memoised can_sum algo with implementation bug

--- src/DYNAMIC_PROGRAMMING/can_sum/can_sum.py
+++ src/DYNAMIC_PROGRAMMING/can_sum/can_sum.py
@@ -1,10 +1,14 @@
-def can_sum(target, numbers):
+def can_sum(target, numbers, memo={}):
+    if target in memo:
+        return memo[target]
     if target < 0:
         return False
     if target == 0:
         return True
 
     for number in numbers:
-        if can_sum(target - number, numbers):
+        if can_sum(target - number, numbers, memo) == True:
+            memo[target] = True
             return True
+    memo[target] = False
     return False
