fbc9ecf58385a7f9be954a8f05017d75bd352dda
Kamil <kulikkamil@gmail.com>
2022-01-06 15:00:20 +0100

    [DP] can_sum experiments cntd

--- src/DYNAMIC_PROGRAMMING/can_sum/can_sum.py
+++ src/DYNAMIC_PROGRAMMING/can_sum/can_sum.py
@@ -1,43 +1,43 @@
-# def can_sum(target, numbers, memo={}):
-#     if target in memo:
-#         return memo[target]
-#     if target < 0:
-#         return False
-#     if target == 0:
-#         return True
-
-#     for number in numbers:
-#         if can_sum(target - number, numbers, memo) == True:
-#             memo[target] = True
-#             return True
-#     memo[target] = False
-#     return False
-
-
-def cache(func):
-    memo = {}
-
-    def worker(*args):
-        hashable_type = [arg for arg in args if type(arg) is int]
-        for arg in hashable_type:
-            if arg not in memo:
-                memo[arg] = func(*args)
-        return memo[hashable_type[0]]
-
-    return worker
-
-
-@cache
-def can_sum(target, numbers):
+def can_sum(target, numbers, memo={}):
+    if target in memo:
+        return memo[target]
     if target < 0:
         return False
     if target == 0:
         return True
 
     for number in numbers:
-        if can_sum(target - number, numbers) == True:
+        if can_sum(target - number, numbers, memo) == True:
+            memo[target] = True
             return True
+    memo[target] = False
     return False
+
+
+# def cache(func):
+#     memo = {}
+
+#     def worker(*args):
+#         hashable_type = [arg for arg in args if type(arg) is int]
+#         for arg in hashable_type:
+#             if arg not in memo:
+#                 memo[arg] = func(*args)
+#         return memo[hashable_type[0]]
+
+#     return worker
+
+
+# @cache
+# def can_sum(target, numbers):
+#     if target < 0:
+#         return False
+#     if target == 0:
+#         return True
+
+#     for number in numbers:
+#         if can_sum(target - number, numbers) == True:
+#             return True
+#     return False
 
 
 #######################################
