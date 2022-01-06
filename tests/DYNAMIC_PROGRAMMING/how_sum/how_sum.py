bac5dc400b89e5c8e6cf4f07a95386ef7ccd0a7f
Kamil <kulikkamil@gmail.com>
2022-01-06 14:04:34 +0100

    [DP] tests for the how_sum algo

---
+++ tests/DYNAMIC_PROGRAMMING/how_sum/how_sum.py
@@ -0,0 +1,19 @@
+import pytest
+from src.DYNAMIC_PROGRAMMING.how_sum.how_sum import how_sum
+
+test_cases = [
+    (7, [2, 4, 6], False),
+    (7, [1, 5, 2], True),
+    (3, [], False),
+    (112, [1, 2, 3], True),
+    (1874, [3, 7, 17], True),
+    (17, [2, 4, 6], False),
+    (300, [7, 14], False),
+]
+
+ids = [f"Target: {case[0]}, Numbers: {case[1]}" for case in test_cases]
+
+
+@pytest.mark.parametrize("target, numbers, sum_possible", test_cases, ids=ids)
+def test_how_sum(target, numbers, sum_possible):
+    assert (type(how_sum(target, numbers)) is list) == sum_possible
