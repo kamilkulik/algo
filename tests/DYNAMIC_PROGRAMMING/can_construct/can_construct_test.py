b08e13fcfd8fccc29d63f131a32345499868fc7e
Kamil <kulikkamil@gmail.com>
2022-01-08 23:00:01 +0100

    [DP] add test cases for the can_construct problem

---
+++ tests/DYNAMIC_PROGRAMMING/can_construct/can_construct_test.py
@@ -0,0 +1,14 @@
+import pytest
+from src.can_construct.can_construct import can_construct
+
+test_cases = [
+    ("skateboard", ["ska", "t", "board", "eboar", "teb"], False),
+    ("abcdef", ["abc", "cde", "def", "bcd", "ef", "a"], True),
+]
+
+ids = ["word: {}".format(case[0]) for case in test_cases]
+
+
+@pytest.mark.parametrize("word, wordbank, constructable", test_cases, ids=ids)
+def test_can_construct(word, wordbank, constructable):
+    assert can_construct(word, wordbank) == constructable
