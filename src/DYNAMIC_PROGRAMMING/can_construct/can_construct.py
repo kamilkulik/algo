f29ed3a7a32a97b18e4bf3adffb936eaf742da1d
Kamil <kulikkamil@gmail.com>
2022-01-08 23:34:42 +0100

    [DP] add memoization to the can_construct algo

--- src/DYNAMIC_PROGRAMMING/can_construct/can_construct.py
+++ src/DYNAMIC_PROGRAMMING/can_construct/can_construct.py
@@ -1,9 +1,13 @@
-def can_construct(word, wordbank):
+def can_construct(word, wordbank, memo={}):
+    if word in memo:
+        return memo[word]
     if word == "":
         return True
     for current_word in wordbank:
         if word.startswith(current_word):
             word_copy = word[len(current_word) :]
-            if can_construct(word_copy, wordbank):
+            if can_construct(word_copy, wordbank, memo):
+                memo[word] = True
                 return True
+    memo[word] = False
     return False
