81b590d9ff6fc3ff593e435450fdf1b717f98936
Kamil <kulikkamil@gmail.com>
2022-01-08 23:34:26 +0100

    [DP] add reset_defaults helper to can_construct tests

--- tests/DYNAMIC_PROGRAMMING/can_construct/can_construct_test.py
+++ tests/DYNAMIC_PROGRAMMING/can_construct/can_construct_test.py
@@ -1,5 +1,6 @@
 import pytest
 from src.DYNAMIC_PROGRAMMING.can_construct.can_construct import can_construct
+from tests.conftest import reset_defaults
 
 test_cases = [
     ("skateboard", ["ska", "t", "board", "eboar", "teb"], False),
@@ -24,4 +25,5 @@ ids = ["word: {}".format(case[0]) for case in test_cases]
 
 @pytest.mark.parametrize("word, wordbank, constructable", test_cases, ids=ids)
 def test_can_construct(word, wordbank, constructable):
+    reset_defaults(can_construct)
     assert can_construct(word, wordbank) == constructable
