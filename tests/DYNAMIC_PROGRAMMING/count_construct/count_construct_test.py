2902a09a1e440cafa0a49015f78fc1b0429f273e
Kamil <kulikkamil@gmail.com>
2022-01-09 13:24:32 +0100

    [DP] add tests for count_construct algo

---
+++ tests/DYNAMIC_PROGRAMMING/count_construct/count_construct_test.py
@@ -0,0 +1,29 @@
+import pytest
+from src.DYNAMIC_PROGRAMMING.count_construct.count_construct import count_construct
+from tests.conftest import reset_defaults
+
+test_cases = [
+    ("skateboard", ["ska", "t", "board", "eboar", "teb"], 0),
+    ("abcdef", ["abc", "ab", "cde", "def", "bcd", "ef", "a", "f"], 3),
+    ("enterapotentpot", ["a", "p", "ent", "enter", "ot", "o", "t"], 4),
+    (
+        "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef",
+        [
+            "e",
+            "ee",
+            "eee",
+            "eeee",
+            "eeeee",
+            "eeeeee",
+        ],
+        0,
+    ),
+]
+
+ids = ["word: {}".format(case[0]) for case in test_cases]
+
+
+@pytest.mark.parametrize("word, wordbank, constructable", test_cases, ids=ids)
+def test_count_construct(word, wordbank, constructable):
+    reset_defaults(count_construct)
+    assert count_construct(word, wordbank) == constructable
