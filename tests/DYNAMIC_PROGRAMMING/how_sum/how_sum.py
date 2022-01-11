b38d044150f0e06f3882ff0bdd933cc9f9a186ac
Kamil <kulikkamil@gmail.com>
2022-01-11 21:26:26 +0100

    [DP] add tests for tabularised version of how_sum

--- tests/DYNAMIC_PROGRAMMING/how_sum/how_sum.py
+++ tests/DYNAMIC_PROGRAMMING/how_sum/how_sum.py
@@ -1,5 +1,5 @@
 import pytest
-from src.DYNAMIC_PROGRAMMING.how_sum.how_sum import how_sum
+from src.DYNAMIC_PROGRAMMING.how_sum.how_sum import how_sum, how_sum_tabularised
 from tests.conftest import reset_defaults
 
 test_cases = [
@@ -19,3 +19,8 @@ ids = [f"Target: {case[0]}, Numbers: {case[1]}" for case in test_cases]
 def test_how_sum(target, numbers, sum_possible):
     reset_defaults(how_sum)
     assert (type(how_sum(target, numbers)) is list) == sum_possible
+
+
+@pytest.mark.parametrize("target, numbers, sum_possible", test_cases, ids=ids)
+def test_how_sum_tabularised(target, numbers, sum_possible):
+    assert (type(how_sum_tabularised(target, numbers)) is list) == sum_possible
