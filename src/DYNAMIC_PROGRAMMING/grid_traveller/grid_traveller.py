97d118e8ce18a14fb2c3b055c364023f6f7b1085
Kamil <kulikkamil@gmail.com>
2022-01-11 07:15:40 +0100

    [DP] final version of the tabularised grid traveller algo

--- src/DYNAMIC_PROGRAMMING/grid_traveller/grid_traveller.py
+++ src/DYNAMIC_PROGRAMMING/grid_traveller/grid_traveller.py
@@ -1,2 +1,12 @@
 def grid_traveller_tabularised(m, n):
-    pass
+    table = [[0 for _ in range(n + 1)] for _ in range(m + 1)]
+    table[1][1] = 1
+
+    for i in range(m + 1):
+        for j in range(n + 1):
+            current = table[i][j]
+            if (j + 1) <= n:
+                table[i][j + 1] += current
+            if (i + 1) <= m:
+                table[i + 1][j] += current
+    return table[m][n]
