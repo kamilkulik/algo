103d7332305ac89e808f7fb93de2ae1bc9077d81
Kamil <kulikkamil@gmail.com>
2022-01-12 07:58:00 +0100

    [DP] tabularised version of the best_sum algo

--- src/DYNAMIC_PROGRAMMING/best_sum/best_sum.py
+++ src/DYNAMIC_PROGRAMMING/best_sum/best_sum.py
@@ -18,3 +18,24 @@ def best_sum(target, numbers, memo={}):
 
     memo[target] = shortest_combination
     return shortest_combination
+
+
+def best_sum_tabularised(target, numbers):
+    table = [None for _ in range(target + 1)]
+    table[0] = []
+
+    for i in range(len(table)):
+        if table[i] is not None:
+            for num in numbers:
+                current_target = i + num
+                if current_target <= target:
+                    current_sum = table[i] + [sum]
+                    table[current_target] = (
+                        current_sum
+                        if (
+                            table[current_target] is None
+                            or len(current_sum) < len(table[current_target])
+                        )
+                        else table[current_target]
+                    )
+    return table[target]
