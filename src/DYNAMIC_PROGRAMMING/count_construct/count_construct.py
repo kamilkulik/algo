53e346e68b8b26e5b0566a66a8e691cacaf19320
Kamil <kulikkamil@gmail.com>
2022-01-16 14:11:51 +0100

    [DP] add tabularised version of the count_construct algo

--- src/DYNAMIC_PROGRAMMING/count_construct/count_construct.py
+++ src/DYNAMIC_PROGRAMMING/count_construct/count_construct.py
@@ -10,3 +10,18 @@ def count_construct(word, wordbank, memo={}):
             combinations += count_construct(suffix, wordbank, memo)
     memo[word] = combinations
     return combinations
+
+
+def count_construct_tabularised(target, wordbank):
+    table = [0 for _ in range(len(target) + 1)]
+    table[0] = 1
+
+    for i in range(len(target)):
+        if table[i]:
+            current_substring = target[i:]
+            for word in wordbank:
+                if i + len(word) <= len(target) and current_substring.startswith(word):
+                    # at this point we have a root that is true
+                    # and we have a word that is found in the current substring
+                    table[i + len(word)] += table[i]
+    return table[-1]
