0f8ce959ff5cafb87bffc91593d67d13a1ea8066
Kamil <kulikkamil@gmail.com>
2022-02-20 13:11:38 +0100

    [GRAPHS] fix river_sizes test suite

---
+++ tests/GRAPHS/river_sizes/river_sizes_test.py
@@ -0,0 +1,22 @@
+import pytest
+from src.GRAPHS.river_sizes.river_sizes import river_sizes
+
+test_cases = [
+    (
+        [
+            [1, 0, 0, 1, 0],
+            [1, 0, 1, 0, 0],
+            [0, 0, 1, 0, 1],
+            [1, 0, 1, 0, 1],
+            [1, 0, 1, 1, 0],
+        ],
+        [2, 1, 5, 2, 2],
+    )
+]
+
+ids = [f"river sizes: {river_sizes}" for _, river_sizes in test_cases]
+
+
+@pytest.mark.parametrize("matrix, river_sizes_list", test_cases, ids=ids)
+def test_river_sizes(matrix, river_sizes_list):
+    assert river_sizes(matrix) == river_sizes_list
