23f40464dc1ec2e3ec4f4fbfbbe9924eea24d146
Kamil <kulikkamil@gmail.com>
2022-02-20 12:37:46 +0100

    [GRAPHS] add river sizes test suite

---
+++ tests/GRAPHS/river-sizes/river-sizes.test.py
@@ -0,0 +1,18 @@
+import pytest
+from src.river-sizes.river-sizes import river-sizes
+
+test_cases = [
+    ([
+    [1, 0, 0, 1, 0],
+    [1, 0, 1, 0, 0],
+    [0, 0, 1, 0, 1],
+    [1, 0, 1, 0, 1],
+    [1, 0, 1, 1, 0]
+  ], [2, 1, 5, 2, 2])
+]
+
+ids = [f"river sizes: {river_sizes}" for river_sizes in test_cases]
+
+@pytest.mark.parametrize("matrix, river_sizes", test_cases, ids = ids)
+def river_sizes_test(matrix, river_sizes):
+    assert river_sizes(matrix) == river_sizes
