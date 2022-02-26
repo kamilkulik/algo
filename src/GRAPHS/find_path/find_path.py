d01ba67280f355fb580ce34b853659b4ca8c1ecb
Kamil <kulikkamil@gmail.com>
2022-02-26 22:14:21 +0100

    [HEAPS] find_shortest_path algo

--- src/GRAPHS/find_path/find_path.py
+++ src/GRAPHS/find_path/find_path.py
@@ -25,3 +25,19 @@ def find_all_paths(graph, start, end, path=[]):
             for new_path in new_paths:
                 paths.append(new_path)
     return paths
+
+
+def find_shortest_path(graph, start, end, path=[]):
+    path = path + [start]
+    if start == end:
+        return path
+    if start not in path:
+        return None
+    shortest_path = None
+    for node in graph[start]:
+        if node not in path:
+            new_path = find_shortest_path(graph, node, end, path)
+            if new_path:
+                if not shortest_path or len(new_path) < len(shortest_path):
+                    shortest_path = new_path
+    return shortest_path
