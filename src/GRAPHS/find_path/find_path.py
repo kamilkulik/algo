b32bb58cd0d0f95d0704a4d2fc8a48c459a6a24f
Kamil <kulikkamil@gmail.com>
2022-02-26 22:01:22 +0100

    [HEAPS] find_all_paths algo

--- src/GRAPHS/find_path/find_path.py
+++ src/GRAPHS/find_path/find_path.py
@@ -10,3 +10,18 @@ def find_path(graph, start, end, path=[]):
             if new_path:
                 return new_path
     return None
+
+
+def find_all_paths(graph, start, end, path=[]):
+    path = path + [start]
+    if start == end:
+        return [path]
+    if start not in path:
+        return []
+    paths = []
+    for node in graph[start]:
+        if node not in path:
+            new_paths = find_all_paths(graph, node, end, path)
+            for new_path in new_paths:
+                paths.append(new_path)
+    return paths
